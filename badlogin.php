58abc3870d70d4419aaa50cdb370adf27d75b585
chilipadiboy <E0032076@u.nus.edu>
2018-07-20 02:24:57 +0000

    First commit

---
+++ badlogin.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="icon" href="img/favicon.ico">
+		<link rel="stylesheet" type="text/css" href="css/login.css">
+		<meta charset="UTF-8">
+		<title>Super Legitimate Login Page</title>
+	</head>
+	<body>
+		<div>
+			<div class="login-box-inner">
+				<div>
+					<img src="img/db-logo.svg" alt="Schenker Logo">
+				</div>
+				<div class="lab" >
+					<label>Sign in</label>
+				</div>
+					<form action="?" method="GET"> 
+					<div>
+						<div class="box">
+							<input type="text" name="user" placeholder="Username"></input>
+						</div>
+						<div class="box">
+							<input type="text" name="password" placeholder="Password"></input>
+						</div>
+					</div>
+					<div>
+						<p>
+							<?php
+								require 'connect.php';
+								if (isset($_GET['login'])) {   #Using get causes input to be displayed in addressbar as plaintext
+									$user = $_GET['user']; 		# not escaping characters lets users input malicious scripts/code
+									$password = $_GET['password'];
+									$error_message = "";
+									if (empty($user)) {
+										$error_message .= "Please enter your username<br>";
+									}if (empty($password)) {
+										$error_message .= "Please enter your password<br>";
+									}
+									if (empty($error_message)) {
+										$sql_select_user = "SELECT * FROM userlist WHERE username='$user'";
+										// Check for sql error
+										if ($conn->multi_query($sql_select_user)) { 
+											do{
+												if ($results = $conn -> store_result()){
+													$res2= $results->fetch_all(MYSQLI_ASSOC);
+													$results->free();
+												}
+											}while ($conn -> more_results() && $conn->next_result());
+											// Check if user is found
+											print_r($res2);
+											echo '<br></br>';
+											if ($res2!=null) {
+													if ($res2[0]['Password']==$password){
+														// Login successful, initializ	e session information
+														if ($res2[0]['Type']==1){
+															echo "Logged in as an <b>ADMINISTRATOR</b>";
+														}else if ($res2[0]['Type']==0){
+															echo "Logged in as a <b>USER</b>";
+														}else{
+															echo "Your account is configured incorrectly";
+														}
+													}else{
+														echo "Wrong password";
+													}
+												$retry = false;
+											} else {
+												echo "Invalid username";
+												$retry = true;
+											}
+										} else {
+											$retry = true;
+										}
+									} else {
+										echo $error_message;
+										$retry = true;
+									}
+								}
+							?>
+						</p>
+					</div>
+					<div>
+						<button type="submit" name="login">Sign in</button>
+					</div>
+			</div>
+		</div>
+		
+	</body>
+</html>
---
+++ connect.php
@@ -0,0 +1,16 @@
+<?php
+$host = "localhost";
+$username = "root";
+$password = "";
+$db = "users";
+// Create connection 
+$conn = new mysqli($host, $username, $password, $db);
+// Check connection
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+}
+$conn->set_charset('utf8mb4');
+if (!($stmt = $conn->prepare("SELECT Username,Password,Type FROM userlist WHERE Username=? AND Password=?"))) { 
+	die("Prepare failed: " . $conn->error); #prepared statments 
+}
+?>
