ea96f728df534b45a09751569dcb41d7f7fe5050
chilipadiboy <E0032076@u.nus.edu>
2018-07-27 11:04:40 +0800

    Add files via upload

--- users.sql
+++ users.sql
@@ -1,54 +1,25 @@
--- phpMyAdmin SQL Dump
--- version 4.8.2
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Jul 20, 2018 at 04:19 AM
--- Server version: 10.1.34-MariaDB
--- PHP Version: 7.2.7
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `users`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `userlist`
---
 
 CREATE TABLE `userlist` (
   `Username` varchar(255) COLLATE utf8_bin NOT NULL,
   `Password` varchar(255) COLLATE utf8_bin NOT NULL,
-  `Type` int(255) NOT NULL
+  `Priviledge` int(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
---
--- Dumping data for table `userlist`
---
-
-INSERT INTO `userlist` (`Username`, `Password`, `Type`) VALUES
+INSERT INTO `userlist` (`Username`, `Password`, `Priviledge`) VALUES
 ('admin', 'password', 1),
-('user', 'hunter2', 0);
+('userXYZ', 'hunter2', 0);
 
---
--- Indexes for dumped tables
---
 
---
--- Indexes for table `userlist`
---
 ALTER TABLE `userlist`
   ADD PRIMARY KEY (`Username`);
 COMMIT;
